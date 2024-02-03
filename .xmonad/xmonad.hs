6bb35025be89733369caa602726f7316868408d2
Francisco Rueda <fruedadev@gmail.com>
2024-02-03 11:10:13 +0100

    Modified the way to run Skype

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -364,7 +364,8 @@ myEventHook = mempty
 -- By default, do nothing.
 myStartupHook = do
     spawnOnce "nitrogen --set-auto --random ~/fondos/NG"
-    spawnOnce "flatpak run com.skype.Client"
+    spawnOnce "skypeforlinux"
+    --spawnOnce "flatpak run com.skype.Client"
     spawnOnce "flatpak run org.signal.Signal"
     spawnOnce "firefox"
     spawnOnce "autokey-gtk"
