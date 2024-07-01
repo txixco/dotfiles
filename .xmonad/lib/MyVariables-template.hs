fcc4f74936ade3bde111d8fa5782d9adcdffabaa
Francisco J. Rueda <39388983+txixco@users.noreply.github.com>
2024-07-01 10:21:49 -0600

    Create MyVariables-template.hs

---
+++ .xmonad/lib/MyVariables-template.hs
@@ -0,0 +1,10 @@
+module MyVariables where
+
+myTerminal     = "terminator"
+myMenu         = "rofi -modi run,drun -show drun -lines 3"
+myPowerMenu    = "rofi -modi p:~/.local/bin/rofi-power-menu -show p -font 'Inconsolata Medium 12'"
+myWindowsMenu  = "rofi -show window"
+myBar          = "xmobar ~/.config/xmobarrc"
+myFilesManager = "nemo"
+myEditor       = "emacs"
+myWallpaper    = "nitrogen --set-zoom-fill --random ~/fondos/NASA"
