bfb8701ec9e04b5c9b8164e13ddad15f1988be58
Francisco Rueda <fruedadev@gmail.com>
2025-04-22 18:30:08 +0200

    Modificaciones a Autokey

--- .config/autokey/data/Modules/wintools.py
+++ .config/autokey/data/Modules/wintools.py
@@ -1,4 +1,5 @@
-from autokey.scripting import System, Window, GtkDialog
+from autokey.scripting import System, Window
+from autokey.scripting.dialog_gtk import GtkDialog
 
 # Constants
 
@@ -16,22 +17,25 @@ class WinTools:
         self.dialog = GtkDialog()
         self.window = Window(mediator)
 
-    def center_window(self, title: str, width_percent: int = 50, shift_percent: int = 0) -> None:
+    def center_window(self, title: str = None, width_percent: int = 50, shift_percent: int = 0) -> None:
         """Center a window in the screen"""
         
+        if not title:
+            title = self.window.get_active_title()
+                
         # Calculate the geometry
         command = "xrandr | grep '*' | awk '{print $1}'"
         (scr_width, scr_height) = map(int, (self.system.exec_command(command)).split("x"))
-        width = scr_width * width_percent / 100
-        height = scr_height * 0.90
+        width = int(scr_width * width_percent / 100)
+        height = int(scr_height * 0.80)
 
         shift = scr_width * shift_percent / 100
-        x = (scr_width / 2) - (width / 2) - shift
-        y = (scr_height / 2) - (height / 2)
+        x = int((scr_width / 2) - (width / 2) - shift)
+        y = int((scr_height / 2) - (height / 2))
         
         # Wait and resize/move
         self.window.wait_for_exist(title, 10)
-        self.window.resize_move(title, 10, 10, width, height)
+        self.window.resize_move(title, x, y, width, height)
 
     def open_url(self, url: str, title: str, width_percent: int = 50) -> None:
         """Open an url with the configured browser"""
---
+++ .config/autokey/data/Scripts/Simple Scripts/NormalWindow.py
@@ -0,0 +1,3 @@
+from wintools import WinTools
+
+WinTools(window.mediator).center_window(width_percent=60)
