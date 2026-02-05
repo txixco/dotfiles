7d13df012385e2abc4fcff0f04942cc788de13cb
Francisco Rueda <frueda@proton.me>
2026-02-05 10:19:26 +0100

    Migration from bare git

---
+++ dot_oh-my-zsh/custom/executable_aliases.zsh
@@ -0,0 +1,9 @@
+alias config="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias wincfg="/usr/bin/git --git-dir=$HOME/wincfg/windotfiles/ --work-tree=$HOME"
+alias ec="$EDITOR $HOME/.zshrc"
+alias sc="source $HOME/.zshrc"
+alias scrum="python3 -c 'import random; persons = [ \"Bo\", \"Chantal\", \"Raúl\", \"Anthony\" ]; random.shuffle(persons); print(\"\\n\".join(persons))'"
+alias upgrade="sudo dnf upgrade -y"
+alias install="sudo dnf install -y"
+alias shutdown="sudo shutdown -h now"
+alias scratch='[[ -z $TMUX ]] && { tmux attach -t scratchpad || exec tmux new -s scratchpad && exit; }'
---
+++ dot_oh-my-zsh/custom/executable_config-aliases.zsh
@@ -0,0 +1,11 @@
+alias cfgst="config status"
+alias cfgpl="config pull"
+alias cfgplr="config pull --rebase"
+alias cfgps="config push"
+alias cfga="config add"
+alias cfgaa="config add -A"
+alias cfgco="config checkout"
+alias cfgcm="config commit -m"
+alias cfgcam="config commit -am"
+
+alias shutdown="sudo shutdown"
